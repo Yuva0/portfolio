bae88208b0307597bbf4245fcb6bf7049310f2e1
Tanuj Sengupta <senguptatanuj@gmail.com>
2024-07-03 16:45:58 +0530

    Modifiation made to getAxios

--- client/src/util/getAxiosRequest.js
+++ client/src/util/getAxiosRequest.js
@@ -4,8 +4,8 @@ const getAxiosRequest = (url) => {
     const axiosInstance = axios.create({baseURL: process.env.REACT_APP_API_URL});
 
     const fetch = async() => {
-        // const res = await axiosInstance.get(url);
-        const res = await axios.get("http://localhost:5000/api/"+url);
+        const res = await axiosInstance.get(url);
+        // const res = await axios.get("http://localhost:5000/api/"+url);
         return res;
     };
     const res = fetch();
