a910686362f7b2a254701f213531952f13e67b60
Tanuj Sengupta <senguptatanuj@gmail.com>
2024-10-07 14:40:51 +0530

    Updated files

--- client/src/components/articlecontent/ArticleBody.js
+++ client/src/components/articlecontent/ArticleBody.js
@@ -1,16 +1,24 @@
 import ReactMarkdown from 'react-markdown'
 import classes from './css/ArticleBody.module.css';
 import { useTheme } from 'stelios';
+import styled from 'styled-components';
+
+const StyledArticleBody = styled.div`
+  font-family: 'Alegreya', serif;
+  & a {
+    color: ${props => props.color};
+  }
+`;
 
 const ArticleBody = (props) => {
   const _color = useTheme().theme.colorPalette.primary.appearance === "light" ? "black" : "white";
-
+  const _primaryColor = useTheme().theme.colorPalette.primary.accentScale[10];
 
   return (
-    <div className={classes.articleBody} style={{color: _color}}>
+    <StyledArticleBody className={classes.articleBody} style={{color: _color}} color={_primaryColor}>
         <ReactMarkdown>{props.content}</ReactMarkdown>
       
-    </div>
+    </StyledArticleBody>
     // Note if you need a new line - Add a newline and then &nbsp; (and two spaces)
     // 
     // &nbsp;  
