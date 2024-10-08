52bfcb5ce2b706efc85abba9ab81f1a59e1d2790
Tanuj Sengupta <senguptatanuj@gmail.com>
2024-10-08 18:36:31 +0530

    Updated Navigation Links

--- client/src/components/layout/Footer/Footer.js
+++ client/src/components/layout/Footer/Footer.js
@@ -1,7 +1,6 @@
 import classes from './css/Footer.module.css';
 import { List, ListItem, Text, useTheme } from 'stelios';
-import { Link } from 'react-router-dom';
-import { Link as SteliosLink } from 'stelios';
+import { Link } from 'stelios';
 import { IconBrandGithub, IconBrandLinkedin } from '@tabler/icons-react';
 
 const Footer = () => {
@@ -15,17 +14,18 @@ const Footer = () => {
                     <div className={classes.footerMenu}>
                         <div className={classes.footerMenuHeader}><Text variant="span" preciseColor='white'>Content</Text></div>
                         <List className={classes.footerMenuContent} style={{padding:"0 0 0 12px", margin: 0, display: "flex", flexDirection: "row", justifyContent: 'center', alignItems:"center"}}>
-                            <ListItem><Link to="/"><Text preciseColor="white">Home</Text></Link></ListItem>
-                            <ListItem><Link to="/projects"><Text preciseColor="white">Projects</Text></Link></ListItem>
-                            <ListItem><Link to="/certificates"><Text preciseColor="white">Certificates</Text></Link></ListItem>
-                            <ListItem><Link to="/skills"><Text preciseColor="white">Skills</Text></Link></ListItem>
+                            {/* <ListItem><Link to="/">>Home</Text></ListItem> */}
+                            <ListItem><Link preciseColor="white" href="/">Home</Link></ListItem>
+                            <ListItem><Link preciseColor="white" href="/projects">Projects</Link></ListItem>
+                            <ListItem><Link preciseColor="white" href="/certificates">Certificates</Link></ListItem>
+                            <ListItem><Link preciseColor="white" href="/skills">Skills</Link></ListItem>
                         </List>
                     </div>
                     <div className={`${classes.footerMenu} ${classes.lastFooterMenu}`}>
                     <div className={classes.footerMenuHeader}><Text variant="span" preciseColor='white'>Socials</Text></div>
                         <ul className={classes.footerMenuContent}>
-                            <SteliosLink preciseColor="white" href="https://www.linkedin.com/in/tanuj-sengupta-872a05129/"><IconBrandLinkedin size="1.75rem"/></SteliosLink>
-                            <SteliosLink preciseColor="white" href="https://github.com/yuva0"><IconBrandGithub size="1.75rem"/></SteliosLink>
+                            <Link preciseColor="white" size="large" href="https://www.linkedin.com/in/tanuj-sengupta-872a05129/" style={{display: "flex", alignItems: "center", justifyContent: "center"}}><IconBrandLinkedin/></Link>
+                            <Link preciseColor="white" size="large" href="https://github.com/yuva0" style={{display: "flex", alignItems: "center", justifyContent: "center"}}><IconBrandGithub/></Link>
                         </ul>
                     </div>
                 </div>
--- client/src/components/layout/Header/Header.js
+++ client/src/components/layout/Header/Header.js
@@ -20,10 +20,9 @@ import {
   IconBrandGithub
 } from "@tabler/icons-react";
 import Settings from "./Settings/Settings";
-import logo from '../../../assets/images/logo.png';
-import name from '../../../assets/images/name.png';
 import { useWindowSize } from "../../../hooks/use-windowsize";
 import Navigation from "./Navigation/Navigation";
+import styled from "styled-components";
 
 const Header = (props) => {
   const colorPalette = useTheme().theme.colorPalette;
@@ -36,7 +35,7 @@ const Header = (props) => {
   const [open, setOpen] = useState(false);
   const [appearance, setAppearance] = useState(colorTokens.appearance);
   const updateTheme = useUpdateTheme();
-  const navigate = useNavigate();
+  const _color = colorPalette.primary.accentScale[10];
 
   const _toggleTheme = () => {
     updateTheme({
@@ -64,32 +63,31 @@ const Header = (props) => {
     >
       <HeaderGroup style={{marginLeft: "1rem"}}>
         {isMobile && <Navigation/>}
-        {!isMobile && <HeaderItem><ReactRouterLink style={{width: "100%", height: "100%"}} to="/"><Text style={{marginLeft: "2rem"}} variant={isLaptop ? "h3" : "h1"} color="primary" fontFamily="'Dosis', sans-serif;">Tanuj Sengupta</Text></ReactRouterLink></HeaderItem>}
+        {!isMobile && <HeaderItem style={{marginLeft: "2rem"}}><Link tabIndex={0} color="primary" style={{width: "100%", height: "100%"}} to="/"><Text tabIndex={-1} variant={isLaptop ? "h3" : "h1"} color="primary" fontFamily="'Dosis', sans-serif;">Tanuj Sengupta</Text></Link></HeaderItem>}
       </HeaderGroup>
       <HeaderGroup></HeaderGroup>
       <HeaderGroup style={{ marginRight: isMobile ? "0.5rem" :"2rem", gap: "1rem" }}>
         {!isMobile && <>
           <HeaderItem>
-            <Link color="primary" onClick={() => navigate("/")}>Home</Link>
+            <Link color="primary" href="/">Home</Link>
           </HeaderItem>
           <HeaderItem>
-            <Link
+            <CategoryLink
+              tabIndex={0}
+              $color={_color}
               color="primary"
               ref={categoryRef}
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "center",
-                alignItems: "center",
-                gap: "0.25rem",
-              }}
-              
               onClick={() => setOpen(!open)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter" || e.key === " ") {
+                  setOpen(!open);
+                }
+              }}
             >
               <Text disableColor>Category</Text>
               <IconCaretDown size="1rem" />
-            </Link>
-            <Menu
+            </CategoryLink>
+            {open && <Menu
               variant="neumorph"
               open={open}
               anchorElement={categoryRef.current}
@@ -110,14 +108,15 @@ const Header = (props) => {
                 ],
               }}
             >
-              <MenuItem><ReactRouterLink to="/projects"><Text color="primary">Projects</Text></ReactRouterLink></MenuItem>
-              <MenuItem><ReactRouterLink to="/certificates"><Text color="primary">Certificates</Text></ReactRouterLink></MenuItem>
-              <MenuItem><ReactRouterLink to="/skills"><Text color="primary">Skills</Text></ReactRouterLink></MenuItem>
+              <MenuItem><Link to="/projects"><Text color="primary">Projects</Text></Link></MenuItem>
+              <MenuItem><Link to="/certificates"><Text color="primary">Certificates</Text></Link></MenuItem>
+              <MenuItem><Link to="/skills"><Text color="primary">Skills</Text></Link></MenuItem>
               {/* <MenuItem><ReactRouterLink to="/projects"><Text color="primary">Cards</Text></ReactRouterLink></MenuItem> */}
-            </Menu>
+            </Menu>}
           </HeaderItem>
           <HeaderItem style={{marginRight: "2rem"}}>
-            <Link  color="primary" onClick={() => navigate("/blogs")}>Blog</Link>
+            <Link color="primary" href="/blogs">Blog</Link>
+            {/* <Link  color="primary" onClick={() => navigate("/blogs")}>Blog</Link> */}
           </HeaderItem>
         </>}
         <HeaderItem>
@@ -149,3 +148,23 @@ const Header = (props) => {
 };
 
 export default Header;
+
+
+const CategoryLink = styled(Link)`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 0.25rem;
+  cursor: pointer;
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  &:focus-visible {
+    outline: 1px solid ${(props) => props.$color};
+    outline-offset: 2px;
+    border-radius: 0.25rem;
+  }
+`;
