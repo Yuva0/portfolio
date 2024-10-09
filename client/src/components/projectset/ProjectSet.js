dd099afda81b89b62a59c8d145b492847fd26f0a
Tanuj Sengupta <senguptatanuj@gmail.com>
2024-10-09 14:03:03 +0530

    Updated filter

--- client/src/components/projectset/ProjectSet.js
+++ client/src/components/projectset/ProjectSet.js
@@ -15,7 +15,8 @@ const ProjectSet = (props) => {
   const params = useParams();
   const navigate = useNavigate();
   const colorPalette = useTheme().theme.colorPalette;
-  const appearance = colorPalette.appearance;
+  const appearance = colorPalette.primary.appearance;
+  const [filteredFrontEnd, setFilteredFrontEnd] = useState("");
 
   useEffect(() => {
     let isMounted = true;
@@ -27,6 +28,9 @@ const ProjectSet = (props) => {
         }
         if(props.limit){
           query = query + "limit=" + props.limit +"&";
+        }
+        if(filteredFrontEnd){
+          query = query + "category=" + filteredFrontEnd + "&";
         }
         if(query){
           const res = await getAxiosRequest("projects/?"+ query);
@@ -44,7 +48,7 @@ const ProjectSet = (props) => {
 
     return () => { isMounted = false };
     
-  }, [props.time, props.type, params.category, props.limit]);
+  }, [props.time, props.type, params.category, props.limit, filteredFrontEnd]);
   
   let currentPageData = posts.map((post,index) => <ProjectSetItem key={index} id={post._id} idTitle={post.idTitle} title={post.title} description={post.description} date={post.buildDate}
   category={post.category} isDetailed = {post.isDetailed} coverImage={post.coverImage} imageAlt={post.imageAlt} difficultyType={post.difficultyType}/>);
@@ -59,12 +63,16 @@ const ProjectSet = (props) => {
     currentPageData = <Text variant='span' color="primary">No Projects found</Text>
   }
 
+  const _handleFrontEndFilter = (e, item) => {
+    setFilteredFrontEnd(item.value);
+  }
+
   return (
     <div>
       <Link variant="hover" color="primary" onClick={() => navigate("/projects")} style={{display: "flex", alignItems: "center", gap: "0.5rem"}}><Text size="large" color="primary">{props.title}</Text> <IconArrowRight/></Link>
         <Text color='black' size="large" style={{marginTop: "1rem"}}>Filters:</Text>
         <div style={{display: "flex", alignItems: "center", flexDirection: "row", flexWrap: "wrap", columnGap: "2rem", marginTop: "0.25rem"}} >
-          <Select placeholder='' size="small" color="black" variant="outlined" label="FrontEnd" options={[{title: "React", value: "react"}, {title: "Typescript", value: "typescript"}]} menuProps={{style: {zIndex: 10000, backgroundColor: appearance === "light" ? "white" : "#202124" }}}/>
+          <Select placeholder='' size="small" color="black" variant="outlined" label="FrontEnd" options={[{title: "React", value: "react"}, {title: "Typescript", value: "typescript"}, {title: "Javascript", value: "javascript"}]} menuProps={{style: {zIndex: 10000, backgroundColor: appearance === "light" ? "white" : "#202124" }}} onClick={_handleFrontEndFilter}/>
           {/* <Select placeholder='' size="small" color="black" variant="outlined" label="BackEnd" options={[{title: "Node", value: "node"}, {title: "Express", value: "express"}]} menuProps={{style: {zIndex: 10000, backgroundColor: appearance === "light" ? "white" : "#202124" }}}/> */}
         </div>
         <div className={classes.projectSetCollection} style={{marginTop: "1rem"}}>
