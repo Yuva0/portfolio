cceca1623ac692b1be775081eaa986447dbb8b5c
Tanuj Sengupta <senguptatanuj@gmail.com>
2024-10-10 08:39:34 +0530

    Updated ATS resume

--- client/src/components/intropage/resume.js
+++ client/src/components/intropage/resume.js
@@ -4,9 +4,8 @@ import classes from "../modals/css/ProjectModal.module.css";
 import colors from "../../tokens/color/color-tokens.json";
 import { IconCross, IconSquareRoundedX, IconX } from '@tabler/icons-react';
 import { Button, NumberInput, Text, ToggleButton, ToggleButtonGroup, useTheme } from 'stelios';
-import resume_vscode from "../../assets/images/resume.png";
-import resume_default from "../../assets/images/resume_default.png";
-
+import resume_vscode from "../../assets/images/resume_vscode.png";
+import resume_ats from "../../assets/images/resume_ats.png";
 export const Backdrop = (props) => {
     return <div style={{position: "fixed", top: 0, left: 0, width: "100%", height: "100vh", zIndex: 6, backgroundColor: "rgba(0, 0, 0, 0.75)"}} onClick={props.onClose}/>
 };
@@ -30,8 +29,8 @@ const Resume = (props) => {
     const _color = useTheme().theme.colorPalette.primary.appearance === "light" ? "black" : "white";
 
     const handleDownloadResume = () => {
-        if (theme === "default") {
-            window.open("https://drive.google.com/uc?export=download&id=1NQFJvtImIGToxfv4jSGGBJtxyHHUtH0d");
+        if (theme === "ats") {
+            window.open("https://drive.google.com/uc?export=download&id=1FcEVmTJKmJqLPiPyG-sLliTbDeV9Kstl");
         }
         else {
             window.open("https://drive.google.com/uc?export=download&id=1eNO7UYY3GoTw6PCwYZXm6Ze5OktcR76p");
@@ -39,8 +38,8 @@ const Resume = (props) => {
     }
 
     const handleFullScreenResume = () => {
-        if (theme === "default") {
-            window.open("https://tanujsengupta.b-cdn.net/resume/default_resume.pdf");
+        if (theme === "ats") {
+            window.open("https://tanujsengupta.b-cdn.net/resume/resume_ats.pdf");
         }
         else {
             window.open("https://tanujsengupta.b-cdn.net/resume/vscode_resume.pdf");
@@ -58,10 +57,10 @@ const Resume = (props) => {
                 </div>
                 <ToggleButtonGroup color="primary" value={theme} width='100%' style={{marginTop: "1.5rem"}} onClick={(e, value) => setTheme(value)}>
                     <ToggleButton value="vscode"><Text disableColor color="primary">VS Code</Text></ToggleButton>
-                    <ToggleButton value="default"><Text disableColor color="primary">Default</Text></ToggleButton>
+                    <ToggleButton value="ats"><Text disableColor color="primary">ATS-friendly</Text></ToggleButton>
                 </ToggleButtonGroup>
                 <div style={{width: "100%", marginTop: "1rem", borderRadius: "8px"}}>
-                    <img style={{width: "100%", borderRadius: "8px"}} src={theme === "vscode" ? resume_vscode: resume_default} alt="resume"/>
+                    <img style={{width: "100%", borderRadius: "8px"}} src={theme === "vscode" ? resume_vscode: resume_ats} alt="resume"/>
                 </div>
                 <div style={{display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", gap: "1rem"}}>
                     <Button variant='neumorph' color='primary' style={{marginTop: "2rem", backgroundColor: colors.accent.primary, color: "white", border:0 }} onClick={handleDownloadResume}><Text disableColor>Download</Text></Button>
