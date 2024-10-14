608ac68df7e2e87f33bb4ede7ac88cb84a27c7a6
Tanuj Sengupta <senguptatanuj@gmail.com>
2024-10-14 18:00:48 +0530

    Updated ATS resume

--- client/src/components/intropage/resume.js
+++ client/src/components/intropage/resume.js
@@ -30,7 +30,7 @@ const Resume = (props) => {
 
     const handleDownloadResume = () => {
         if (theme === "ats") {
-            window.open("https://drive.google.com/uc?export=download&id=1FcEVmTJKmJqLPiPyG-sLliTbDeV9Kstl");
+            window.open("https://drive.google.com/uc?export=download&id=1PONvnsGiqQ-kz2wRH4c9ANDAcfO4IPNx");
         }
         else {
             window.open("https://drive.google.com/uc?export=download&id=1eNO7UYY3GoTw6PCwYZXm6Ze5OktcR76p");
